b5f989216fe14b474af3b8330dd0831cc19526d5
gowrishankkar <shankarg.kvsulur@gmail.com>
2021-05-11 22:47:17 +0530

    Progress bar created

---
+++ src/comps/ProgressBar.js
@@ -0,0 +1,6 @@
+import React from "react";
+import useStorage from "../hooks/useStorage";
+
+const progressBar = () => {
+  return <div className="progress-bar"> Progress</div>;
+};
--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import ProgressBar from "../comps/ProgressBar";
 
 export const UploadForm = () => {
   const [file, setFile] = useState(null);
@@ -22,6 +23,8 @@ export const UploadForm = () => {
       <input type="file" onChange={changeHandler} />
       <div className="output">
         {error && <div className="error">{error}</div>}
+        {file && <div>{file.name}</div>}
+        {file && <ProgressBar file={file} setFile={setFile} />}
       </div>
     </form>
   );
